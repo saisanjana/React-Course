e96a105bbb3ab03cf7afa9e1d0f1c1b7d08c4658
Sanjana <gsaisanjana21@gmail.com>
2020-06-01 15:00:30 +0530

    added functional components

--- src/components/dishDetail.js
+++ src/components/dishDetail.js
@@ -1,46 +1,67 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText, Media } from 'reactstrap';
-class DishDetails extends Component {
-    constructor(props) {
-        super(props);
+import React from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+
+
+function RenderContent({ dis }) {
+    if (dis == null) {
+        return (
+            <div></div>
+        );
     }
-    render() {
-        const dis = this.props.dish;
-        var content = <div></div>;
-        var comments = <div></div>;
-        var head=<div></div>;
-        const month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-        if (dis != null) {
-            content =
-                <Card>
-                    <CardImg top src={dis.image} alt={dis.name} />
-                    <CardBody>
-                        <CardTitle>{dis.name}</CardTitle>
-                        <CardText>{dis.description}</CardText>
-                    </CardBody>
-                </Card>;
-            comments = dis.comments.map((c) => {
-                return (
-                    <div className="mt-3">
-                        <p>{c.comment}</p>
-                        <p>--{c.author} , {new Intl.DateTimeFormat("en-US", {
-                            year: "numeric",
-                            month: "long",
-                            day: "2-digit"
-                        }).format(new Date(c.date))}</p>
-                    </div>
-                );
-            });
-            head=<h3>Comments</h3>
-        }
-        return <div className="container">
+    return (
+        <Card>
+            <CardImg top src={dis.image} alt={dis.name} />
+            <CardBody>
+                <CardTitle>{dis.name}</CardTitle>
+                <CardText>{dis.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+function RenderCommentshead({ dis }) {
+    if (dis == null) {
+        return (
+            <div></div>
+        );
+    }
+    return (
+        <h3>Comments</h3>
+    );
+}
+function RenderComments({ c }) {
+    return (
+        <div className="mt-3">
+            <p>{c.comment}</p>
+            <p>--{c.author} , {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "2-digit"
+            }).format(new Date(c.date))}</p>
+        </div>
+    );
+}
+
+const DishDetails = (props) => {
+    const dis = props.dish;
+    var comments=<div></div>;
+    if (dis != null) {
+        comments = dis.comments.map((c) => {
+            return (
+                <RenderComments c={c} />
+            );
+        });
+
+    }
+    return (<div className="container">
         <div className="row">
-            <div className="col-12 col-md-5 m-1">{content}</div>
+            <div className="col-12 col-md-5 m-1"><RenderContent dis={dis} /></div>
             <div className="col-12 col-md-5 m-1">
-                {head}
+                <RenderCommentshead dis={dis} />
                 {comments}
             </div>
-        </div></div>;
-    }
+        </div>
+    </div>);
 }
+
+
 export default DishDetails;
